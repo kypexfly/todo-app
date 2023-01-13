cd46288244010ab2c7c9580f99904eb6e9be8581
Ricardo Chu <92066360+kypexfly@users.noreply.github.com>
2023-01-13 18:26:27 -0500

    Remove functions from old state management system

--- src/components/TodoContainer.tsx
+++ src/components/TodoContainer.tsx
@@ -4,33 +4,6 @@ import TodoForm from '../components/TodoForm'
 import TodoList from '../components/TodoList'
 
 const TodoContainer = () => {
-  // const [todos, setTodos] = useState<Todo[]>()
-
-  // const addTodo = (newTodo: Todo) => {
-  //   setTodos((todos) => [newTodo, ...todos])
-  // }
-
-  // const deleteTodo = (id: number) => {
-  //   setTodos((todos) => todos.filter((todo) => todo.id !== id))
-  //   toast.info('Deleted task')
-  // }
-
-  // const toggleCompleted = (id: number) => {
-  //   const updatedTodos = todos.map((todo) => {
-  //     if (todo.id === id) return { ...todo, completed: !todo.completed }
-  //     return todo
-  //   })
-  //   setTodos(updatedTodos)
-  // }
-
-  // const toggleFavorite = (id: number) => {
-  //   const updatedTodos = todos.map((todo) => {
-  //     if (todo.id === id) return { ...todo, favorite: !todo.favorite }
-  //     return todo
-  //   })
-  //   setTodos(updatedTodos)
-  // }
-
   return (
     <main className='min-h-screen'>
       <TodoForm />
